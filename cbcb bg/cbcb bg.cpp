6da0b62ff3cfe26b911fb9abc0c4fea73e7d7579
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-08 17:09:35 +0700

    cb deh

--- cbcb bg/cbcb bg.cpp
+++ cbcb bg/cbcb bg.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim(); //deklarasi method
+};
+
+
+void mahasiswa::showNim() { //implementasi method diluar class
+	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };		//object mhs
+	mhs.showNim();			//member access operator
+
+	mahasiswa* pMhs = &mhs;	//pointer reference refMHs
+	pMhs->nim = 3;			//arrow operator
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
 
